217d8f7938ee53560fad1e52be7f8c77778a94ae
Jamie Klassen <michigan.j.frog@gmail.com>
2013-09-04 15:48:18 -0400

    point setup.sh to correct directories

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 sudo yum localinstall -y http://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/epel-release-6-5.noarch.rpm
 sudo yum localinstall -y http://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/ius-release-1.0-11.ius.centos6.noarch.rpm
 sudo yum install -y python27 python27-distribute gcc qt PyQt4
-cd /vagrant
+cd /vagrant/vis
 sudo pip-2.7 install -r requirements.txt
-cd vis/django_vis
+cd /vagrant/django_vis
 sudo pip-2.7 install -r requirements.txt
 sudo python2.7 manage.py runserver [::]:80

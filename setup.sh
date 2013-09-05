50a10b09ebb8df036b6555aa586d9446af80ae6c
Jamie Klassen <michigan.j.frog@gmail.com>
2013-09-05 16:58:55 -0400

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 sudo yum localinstall -y http://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/epel-release-6-5.noarch.rpm
 sudo yum localinstall -y http://dl.iuscommunity.org/pub/ius/stable/CentOS/6/x86_64/ius-release-1.0-11.ius.centos6.noarch.rpm
 sudo yum install -y python27 python27-distribute gcc qt PyQt4
+sudo easy_install-2.7 -U pip
 cd /vagrant/vis
 sudo pip-2.7 install -r requirements.txt
 cd /vagrant/django_vis
